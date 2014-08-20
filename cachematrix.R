f3d7ecff5ecc7627d7c2e2b8f7dae26e2d634a11
nonogit <rroitman@gmail.com>
2014-08-20 10:46:03 -0300

    Final solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -19,7 +15,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
         m <- x$getmean()
